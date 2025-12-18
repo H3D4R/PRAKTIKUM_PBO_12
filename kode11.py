57dba6fff1894a6ccc272dde708a73670435f575
HED4R <edharboy@gmail.com>
2025-12-18 13:53:11 +0800

    Docs Add Google-style docstrings to interfaces and services

--- kode11.py
+++ kode11.py
@@ -156,4 +156,3 @@ checkout_qris = CheckoutService(payment_processor=qris_processor, notifier=email
 
 LOGGER.info("--- Skenario 2: Pembuktian OCP (QRIS) ---")
 checkout_qris.run_checkout(budi_order)
-
